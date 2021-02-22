1149b2f320b41ec59cb73a86dbd255697b345e6e
user <user@DESKTOP-F4RGEK8>
2021-02-22 16:44:43 -0300

    Tudo funcionando , alterar , deletar , inserir e ler

---
+++ Modelo/Pessoa.php
@@ -0,0 +1,56 @@
+<?php
+
+require 'Conexao.php';
+
+class Pessoa extends Conexao {
+
+    public function __construct() {
+        $this->conexao = Conexao::conexao();
+    }
+
+    public function inserir($nome, $idade, $cpf, $telefone, $sexo, $endereco) {
+
+        $sql = "insert into usuario (nome, idade, cpf, telefone, sexo, endereco) values (?, ?, ?, ?, ?, ?);";
+        $con = self::conexao()->prepare($sql);
+        $con->bindValue(1, $nome);
+        $con->bindValue(2, $idade);
+        $con->bindValue(3, $cpf);
+        $con->bindValue(4, $telefone);
+        $con->bindValue(5, $sexo);
+        $con->bindValue(6, $endereco);
+
+        $con->execute();
+    }
+
+    public function alterar($nome, $idade, $cpf, $telefone, $sexo, $endereco, $codigo) {
+
+        $sql = "UPDATE usuario SET nome=?, idade=?, cpf=?, telefone=?, sexo=?, endereco=? where codigo =?;";
+        $c = self::conexao()->prepare($sql);
+        $c->bindValue(1, $nome);
+        $c->bindValue(2, $idade);
+        $c->bindValue(3, $cpf);
+        $c->bindValue(4, $telefone);
+        $c->bindValue(5, $sexo);
+        $c->bindValue(6, $endereco);
+        $c->bindValue(7, $codigo);
+
+        $c->execute();
+    }
+
+    public function deletar($codigo) {
+
+        $sql = "DELETE from usuario where codigo =?;";
+        $c = self::conexao()->prepare($sql);
+        $c->bindValue(1, $codigo);
+        $c->execute();
+    }
+
+    public function ler() {
+
+        $sql = "Select * from usuario;";
+        $c = self::conexao()->query($sql);
+        $resposta = $c->fetchAll(PDO::FETCH_ASSOC);
+        return $resposta;
+    }
+
+}
---
+++ alterar.php
@@ -0,0 +1,146 @@
+<?php
+require './Modelo/Pessoa.php';
+
+$callClass = new Pessoa();
+
+
+$data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+$param = filter_input_array(INPUT_GET, FILTER_DEFAULT);
+
+
+if (isset($data['alterar'])) {
+
+    $callClass->alterar($data['nome'], $data['idade'], $data['cpf'], $data['telefone'], $data['sexo'], $data['endereco'], $param['codigo']);
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+
+        <link rel="stylesheet" href="css/style.css"/>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src='js/jquery.mask.js' ></script>
+
+    </head>
+
+
+    <body>
+
+
+        <h1 id="titulo">Alterando o Cadastro</h1>
+        <form action="alterar.php?codigo=<?php echo $param['codigo'] ?>" method="post">
+            <table width=300 height=100 align=center>
+                <tr>
+                    <td>Nome</td>
+                    <td align="center" id="cad">
+                        <input id="nome" class='campo' type="text" name="nome" placeholder="Insira seu nome">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Idade</td>
+                    <td align="center">
+                        <input id="idade" class='campo' type="text"  name="idade" placeholder="Insira a idade">
+                    </td>
+                </tr>
+                <tr>
+                    <td>CPF</td>
+                    <td align="center">
+                        <input id="cpf" class='campo' type="text"  name="cpf"  placeholder="Insira o CCPF">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Telefone</td>
+                    <td align="center">
+                        <input id="telefone" class='campo' type="text"  name="telefone" placeholder="Insira o numero de telefone">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Sexo</td>
+                    <td align="center">
+                        <input id="sexo" class='campo' type="text"  name="sexo" placeholder="Insira seu sexo ( M / F )">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Endereco</td>
+                    <td align="center">
+                        <input id="endereco" class='campo' type="text"  name="endereco" placeholder="Insira seu endereço">
+                    </td>
+                </tr>
+            </table>
+            <button type="submit" id="botao"  name="alterar">Alterando</button>
+
+        </form>
+        <br><br>
+
+        <h1 id="titulo">Lista de Cadastrados</h1>
+        <table border="5" width=300 height=100 align=center>
+            <thead>
+                <tr id="tabela">
+                    <th>Codigo</th>
+                    <th>Nome</th>
+                    <th>Idade</th>
+                    <th>Cpf</th>
+                    <th>Telefone</th>
+                    <th>Sexo</th>
+                    <th>Endereço</th>
+                    <th>Ação</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $usuarios = $callClass->ler();
+                foreach ($usuarios as $usuario):
+                    ?>
+                    <tr id="linhas">  
+                        <td>
+                            <?= $usuario["codigo"] ?>
+                        </td>
+                        <td>
+                            <?= $usuario["nome"] ?>
+                        </td>
+                        <td>
+                            <?= $usuario["idade"] ?>
+                        </td>
+                        <td>
+                            <?= $usuario["cpf"] ?>
+                        </td>
+                        <td>
+                            <?= $usuario["telefone"] ?>
+                        </td>
+                        <td>
+                            <?= $usuario["sexo"] ?>
+                        </td>
+                        <td>
+                            <?= $usuario["endereco"] ?>
+                        </td>
+                        <td> 
+                            <a href="excluir.php?codigo=<?= $usuario['codigo'] ?>"><font color=red>Deletar</font></a>
+                            <a href="alterar.php?codigo=<?= $usuario['codigo'] ?>"><font color=red>Alterar</font></a>
+                        </td>
+                    </tr>
+                    <?php
+                endforeach;
+                ?>
+        </table>
+        <a href="index.php"><button id="botaoVoltar">Voltar</button></a>
+    </tbody>
+
+
+
+
+
+
+    <script>
+        $('.cpf').mask('000.000.000-00', {reverse: true});
+        $('.telefone').mask('(00) 0000-0000');
+    </script>
+
+
+
+
+
+
+</body>
+</html>
